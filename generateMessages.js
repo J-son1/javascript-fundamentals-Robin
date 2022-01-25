fdd3ee52dfafb4f61805508471d74af6600a2ad9
Jason Humbert <jasonhumbert@outlook.com>
2022-01-25 16:09:37 +0000

    Update generateMessages to use an object

--- generateMessages.js
+++ generateMessages.js
@@ -1,9 +1,15 @@
-const generateMessages = names => {
-  return names.map(name => {
-    return `Hi ${name}! 50% off our best candies for you today!`;
+const generateMessages = namesAndDiscounts => {
+  return namesAndDiscounts.map(customer => {
+    return `Hi ${customer.name}! ${customer.discount}% off our best candies for you today!`;
   });
 };
 
-const names = ["Anna", "Laura", "Josh", "Min", "Karla"];
+const namesAndDiscounts = [
+  { name: 'Anna', discount: 50 },
+  { name: 'Laura', discount: 40 },
+  { name: 'Josh', discount: 30 },
+  { name: 'Min', discount: 50 },
+  { name: 'Karla', discount: 60 }
+];
 
-console.log(generateMessages(names));
+console.log(generateMessages(namesAndDiscounts));
