d80434ff7a57402fc1fb87923f31900cfb414e97
Jason Humbert <jasonhumbert@outlook.com>
2022-01-25 14:50:01 +0000

    Add addToBatch

---
+++ addToBatch.js
@@ -0,0 +1,15 @@
+const addToBatch = (array, number) => {
+  if (array.length <= 5) {
+    return array.concat(number);
+  }
+
+  return array;
+}
+
+console.log(addToBatch([1, 3], 3));
+
+console.log(addToBatch([1, 2, 3], 4)); 
+
+console.log(addToBatch([], 8));
+
+console.log( addToBatch([1, 2, 3, 4, 5, 6], 7));
